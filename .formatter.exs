e13fc36242e96e8d69ab86e98f787e92fc202b87
Alex McLain <alex@alexmclain.com>
2022-12-27 22:54:24 -0800

    Disable formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,3 +1,13 @@
+IO.puts """
+\u001b[33mFormatter disabled.\u001b[0m
+This repository uses a style guide.
+
+See:
+  https://github.com/amclain/styleguides/blob/master/elixir/STYLEGUIDE.md
+"""
+
+Kernel.exit(:normal)
+
 # Used by "mix format"
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
