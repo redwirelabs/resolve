9da4de2b7e5b67c07f7fa94b2916efa3ef8388d6
Alex McLain <alex@alexmclain.com>
2022-12-27 22:57:57 -0800

    Add dialyzer

---
+++ dialyzer.ignore.exs
@@ -0,0 +1,3 @@
+[
+  {"lib/di.ex", :pattern_match, 90},
+]
--- mix.exs
+++ mix.exs
@@ -12,6 +12,11 @@ defmodule Resolve.MixProject do
       deps: deps(),
       docs: docs(),
       start_permanent: Mix.env() == :prod,
+      dialyzer: [
+        ignore_warnings: "dialyzer.ignore.exs",
+        list_unused_filters: true,
+        plt_file: {:no_warn, plt_file_path()},
+      ],
     ]
   end
 
@@ -32,6 +37,7 @@ defmodule Resolve.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:dialyxir, "~> 1.2", only: :dev, runtime: false},
       {:ex_doc, "~> 0.29", only: :dev, runtime: false},
     ]
   end
@@ -71,5 +77,12 @@ defmodule Resolve.MixProject do
       || raise "Could not find executable 'open' or 'xdg-open'"
 
     System.cmd(open_command, [file])
+  end
+
+  # Path to the dialyzer .plt file.
+  defp plt_file_path do
+    [Mix.Project.build_path(), "plt", "dialyxir.plt"]
+    |> Path.join()
+    |> Path.expand()
   end
 end
